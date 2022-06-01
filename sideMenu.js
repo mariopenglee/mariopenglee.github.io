3be9ba2f0ae93af205d9ef0e8a991e49d5ccdc27
MariusPL <mario.penglee.mpl@gmail.com>
2022-05-31 21:26:38 -0700

    s

--- sideMenu.js
+++ sideMenu.js
@@ -10,6 +10,6 @@
         function closeNav() {
           document.getElementById("mySidenav").style.width = "0";
           document.getElementById("main").style.marginLeft = "0";
-          document.body.style.backgroundColor = "white";
+          document.body.style.backgroundColor = "black";
         }
  
